6ca4b39f77f2482bd26bb6406c8ee8bfd74e90a8
aistomin <andrej.istomin.ikeen@gmail.com>
2016-09-11 15:31:54 +0200

    application skeleton

---
+++ grails-app/conf/UrlMappings.groovy
@@ -0,0 +1,12 @@
+class UrlMappings {
+
+	static mappings = {
+        "/$controller/$action?/$id?(.$format)?"{
+            constraints {
+            }
+        }
+
+        "/"(view:"/index")
+        "500"(view:'/error')
+	}
+}
